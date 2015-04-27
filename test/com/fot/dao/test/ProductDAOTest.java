3c821c66ada545d7a745b75c20ab97277c0b2353
ganesh <ganeshr89@yahoo.co.in>
2015-04-27 09:28:47 -0400

    Added Test Case for product admin fn

--- test/com/fot/dao/test/ProductDAOTest.java
+++ test/com/fot/dao/test/ProductDAOTest.java
@@ -10,23 +10,39 @@ import com.fot.model.Product;
 
 public class ProductDAOTest {
 	
+
 	@Test
 	public void getAllTest() {
 		List<Product> products = ProductDAO.getAll();
 		Assert.assertTrue(products.size() > 0);
 	}
-	
+
 	@Test
 	public void getProductByCodeTest() {
 		Product product = ProductDAO.getProductByCode("PR1");
 		Assert.assertNotNull(product);
 	}
-	
+
 	@Test
 	public void getProductByCategoryTest() {
 		List<Product> products = ProductDAO.getProductByCategory("bakery");
 		Assert.assertNotNull(products);
 		Assert.assertTrue(products.get(0).getCategory().equals("bakery"));
 	}
-	
+
+	@Test
+	public void adminProductTest() {
+		Product pro1 = new Product(
+				"PR69",
+				"Blueberry Scone",
+				"bakery",
+				"The light biscuit texture provides the ideal backdrop for a bounty of dried blueberries and lemon zest. Sweet yet tart, our classic Blueberry Scone calls for a balanced coffee, such as House Blend®.",
+				"http://globalassets.starbucks.com/assets/ea8adb19d1894a7e930b218c0112c357.jpg", 2.5);
+		ProductDAO.saveProduct(pro1);
+		Assert.assertNotNull(ProductDAO.getProductByCode("PR68"));
+		pro1.setCategory("fastfood");
+		ProductDAO.updateProduct(pro1);
+		Assert.assertNotNull(ProductDAO.getProductByCategory("fastfood"));
+		ProductDAO.deleteProductByCode("PR69");
+	}
 }
